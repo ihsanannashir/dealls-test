62e99dcc43d33387b21b7f19f1b512ac3cae2707
ihsanannashir <ihsanannashir@gmail.com>
2024-10-09 15:29:25 +0700

    setup tailwind brand color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        brand: "#6914D8",
       },
     },
   },
