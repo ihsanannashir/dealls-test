aec0d2c19d70bcab013448c671410380a684b9a1
ihsanannashir <ihsanannashir@gmail.com>
2024-10-09 16:22:43 +0700

    import modules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
