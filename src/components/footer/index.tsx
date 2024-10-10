2cc29ac6ce46a855412d64bb8d532c0d53695647
ihsanannashir <ihsanannashir@gmail.com>
2024-10-10 14:15:12 +0700

    add footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <p className="text-center text-sm text-gray-600">
+      Copyright Ihsan An-Nashir 2024 - All Right Reserved.
+    </p>
+  );
+};
+
+export default Footer;
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,3 +1,4 @@
+import Footer from "../footer";
 import NavigationBar from "../navigation-bar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -5,6 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <NavigationBar />
       <main className="max-w-6xl sm:mx-auto mt-20 mb-10 px-4">{children}</main>
+      <Footer />
     </>
   );
 }
