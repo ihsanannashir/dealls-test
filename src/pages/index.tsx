514d451c90407844e5189b0b1c48cf6c7828b569
ihsanannashir <ihsanannashir@gmail.com>
2024-10-10 09:40:32 +0700

    update landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,10 @@ const Home = ({ articles }: HomeProps) => {
         <meta name="keywords" content="jobs, mentoring, Indonesia, Dealls" />
       </Head>
 
+      <h1 className="font-semibold text-xl text-center mb-10">
+        Discover job opportunities and mentoring for Indonesian talents!
+      </h1>
+
       <div className="grid grid-cols-3 gap-4">
         <div className="col-span-2">
           <Swiper
@@ -30,24 +34,35 @@ const Home = ({ articles }: HomeProps) => {
             slidesPerView={1}
             pagination
             autoplay
-            className="h-60"
+            className="h-60 rounded-lg"
           >
-            {articles.data.data.map((article) => (
-              <SwiperSlide key={article.id}>
-                <ArticleCard data={article} />
-              </SwiperSlide>
-            ))}
+            {articles.data.data.map((article, index) => {
+              if (index < 5) {
+                return (
+                  <SwiperSlide key={index}>
+                    <ArticleCard data={article} />
+                  </SwiperSlide>
+                );
+              }
+            })}
           </Swiper>
         </div>
-        <div className="space-y-4 border">Dummy</div>
+        <div className="space-y-4">
+          {articles.data.data.map((article, index) => {
+            if (index > 4) {
+              return <ArticleCard key={index} data={article} variant="small" />;
+            }
+          })}
+        </div>
       </div>
     </>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const articles = await fetchArticles();
+  const articles = await fetchArticles(undefined, undefined, 8);
   const categories = await fetchCategories();
+
   return { props: { articles, categories } };
 };
 
